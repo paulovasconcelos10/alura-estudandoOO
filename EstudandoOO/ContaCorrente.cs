370b7601103a953c3f0d7f19838b69a3d7441839
Paulo <paulovasconcelosbr@hotmail.com>
2020-05-28 22:10:58 -0300

    Estudando OO no Alura 1

---
+++ EstudandoOO/ContaCorrente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudandoOO
+{
+    public class ContaCorrente
+    {
+        public String titular;
+        public int conta;
+        public int agencia;
+        public double saldo;
+
+
+
+        public bool Sacar (double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Depositar(double valor) {
+            this.saldo += valor;
+        
+        }
+
+    }
+}
---
+++ EstudandoOO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EstudandoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta1 = new ContaCorrente();
+
+            conta1.titular = "Paulo";
+            conta1.Depositar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta2.titular = "Victor";
+            conta2.Depositar(100);
+
+
+            Console.WriteLine(conta1.titular + " Saldo: " + conta1.saldo);
+            if (conta1.Sacar(150))
+            {
+                Console.WriteLine("Saque realizado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Sem saldo");
+            }
+
+            Console.WriteLine(conta1.titular + " Saldo: " + conta1.saldo);
+
+            Console.WriteLine(conta2.titular + " Saldo: " + conta2.saldo);
+            if (conta2.Sacar(150))
+            {
+                Console.WriteLine("Saque realizado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Sem saldo");
+            }
+
+            Console.WriteLine(conta2.titular + " Saldo: " + conta2.saldo);
+            Console.ReadLine();
+  
+        }
+    }
+}
